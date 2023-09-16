8df01e16a5420ac0d9de2f8126787048d7316fde
Alex Song <alex@aysong.dev>
2023-09-16 01:46:05 -0700

    Render and reduce EventBus scope

---
+++ src/GameState.js
@@ -0,0 +1,2 @@
+// Global DEBUG_MODE flag
+let DEBUG_MODE = false;
---
+++ src/Render.js
@@ -0,0 +1,35 @@
+const Render = {
+    start: function() {
+        // Create canvas element
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d")
+        canvas.id = "gameArea";
+        canvas.style.position = "fixed";
+        canvas.style.top = "50%";
+        canvas.style.left = "50%";
+        canvas.style.transform = "translate(-50%, -50%)";
+        document.body.appendChild(canvas);
+        
+        // background color 
+        document.body.style.backgroundColor = "grey";
+
+        Render.resizeCanvas(canvas);
+
+        function render() {
+            ctx.fillStyle = "black";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            requestAnimationFrame(render);
+        }
+        render();
+    },
+
+    resizeCanvas: function(canvas) {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        EventBus.publish("ResizeCanvas", canvas);
+    },
+
+};
+
+EventBus.subscribe("GameStart", Render.start);
