68a940d6bbb2b2aeea248aeddf6e4af3ce03a5fb
unknown <wescjmc@gmail.com>
2025-02-18 20:07:51 +0700

    added scalaing grid

--- src/Render.js
+++ src/Render.js
@@ -16,10 +16,50 @@ const Render = {
 
         Render.resizeCanvas(canvas);
 
+        //Stuff to make this work as a test
+        let dark = true;
+        let game = {
+            size: [30, 30]
+        }
+        const scale = Math.min(canvas.width * 0.8 / game.size[0], canvas.height * 0.8 / game.size[1]);
+
         function render() {
             ctx.fillStyle = "black";
             ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+            // draw grid
+            ctx.strokeStyle = dark ? "#666" : "#ccc";
+            ctx.lineWidth = 1;
+            let draw_cell_direction = false;
+            let d = 0.15;
+            for (let x=0; x<=game.size[0]; ++x) {
+                ctx.lineWidth = x%2 ? 0.8 : 2;
+                ctx.beginPath();
+                ctx.moveTo(x*scale, 0);
+                if (draw_cell_direction && x > 0 && x < game.size[0]) {
+                for (let y=0; y<game.size[1]; ++y) {
+                    ctx.lineTo(x*scale, (y+0.5-d)*scale);
+                    ctx.lineTo((x+(y%2?d:-d))*scale, (y+0.5)*scale);
+                    ctx.lineTo(x*scale, (y+0.5+d)*scale);
+                }
+                }
+                ctx.lineTo(x*scale, game.size[1]*scale);
+                ctx.stroke();
+            }
+            for (let y=0; y<=game.size[1]; ++y) {
+                ctx.lineWidth = y%2 ? 0.8 : 2;
+                ctx.beginPath();
+                ctx.moveTo(0, y*scale);
+                if (draw_cell_direction && y > 0 && y < game.size[1]) {
+                for (let x=0; x<game.size[0]; ++x) {
+                    ctx.lineTo((x+0.5-d)*scale, y*scale);
+                    ctx.lineTo((x+0.5)*scale, (y+(x%2?-d:d))*scale);
+                    ctx.lineTo((x+0.5+d)*scale, y*scale);
+                }
+                }
+                ctx.lineTo(game.size[0]*scale, y*scale);
+                ctx.stroke();
+            }
             requestAnimationFrame(render);
         }
         render();
@@ -29,6 +69,8 @@ const Render = {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
         EventBus.publish("ResizeCanvas", canvas);
+        
+        
     },
 
 };
